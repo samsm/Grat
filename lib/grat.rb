1be7c6c62cdb16eec7025e0375483c4289e4b18e
Sam Schenkman-Moore <samsm@samsm.com>
2009-10-25 16:06:25 -0400

    A great deal of meandering.

--- lib/grat.rb
+++ lib/grat.rb
@@ -1,9 +1,64 @@
+require 'ruby-debug'
 require 'sinatra'
+require 'haml'
+require 'sass'
+require 'mongomapper'
+MongoMapper.database = 'grat_development'
+
+require File.dirname(__FILE__) + '/grat/content'
+
 
 module Grat
   class Application < Sinatra::Base
-    get '/' do
-      'hi'
+    get '/favicon.ico' do
+      nil
+    end
+    
+    get '/:name.css' do
+      content_type 'text/css', :charset => 'utf-8'
+      sass "css/_#{params[:name]}".to_sym
+    end
+    
+    get '/admin/*' do
+      haml :content_form
+    end
+    
+    post '/admin/*' do
+      content.update_attributes(focus_params)
+      redirect "/admin#{content.url}"
+    end
+    
+    get '/*' do
+      haml(content.content) #or missing_page
+    end
+    
+    def url
+      '/' + params[:splat].join('/')
+    end
+    
+    def content
+      @content ||= Content.find_by_url(url) || Content.new(:url => url)
+    end
+    
+    def focus ; 'content' ; end
+    
+    def focus_params
+      params[focus].reject {|k,v| k == 'submit'}
+    end
+    
+    def missing_page
+      haml :missing
+    end
+    
+    
+    
+    helpers do
+      def form_nest(name,context = focus)
+        "#{context}[#{name}]"
+      end
+      def stylesheet_tag(href)
+        "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\"  href=\"/#{href}\" />"
+      end
     end
     
   end
---
+++ lib/grat/content.rb
@@ -0,0 +1,17 @@
+class Content
+  include MongoMapper::Document
+  
+  key :url, String
+  key :tags, Array
+  key :content, String
+  timestamps!
+  
+  def editable_fields
+    attributes.reject {|k,v| uneditable_keys.include? k }
+  end
+  
+  def uneditable_keys
+    # url is in here so it can maually be placed at the top of edit form.
+    ["updated_at", "_id", "url", "created_at"]
+  end
+end

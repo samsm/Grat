192e622f264cf1b2b6e42ea8bd825818fd9156b6
Sam Schenkman-Moore <samsm@samsm.com>
2009-10-28 15:41:06 -0400

    Detect variables needed in haml.

--- lib/grat/template.rb
+++ lib/grat/template.rb
@@ -17,4 +17,29 @@ class Grat::Template
     super(val =~ BEGINS_WITH_TEMPLATE ? val : '/templates/' + val.sub(/\A\//,''))
   end
   
+  def detect_variables_needed_by_content
+    haml = Haml::Engine.new(content)
+    variables_needed = {}
+    # formats = [String,Array] # later
+    render_fails = true
+    counter = 0
+    while render_fails
+      counter += 1 
+      return false if counter > 200 # no infinite loop, thanks
+      begin
+        content_with(variables_needed)
+        render_fails = false
+      rescue
+        var = $!.to_s.sub(/.+`/,'').sub(/'.+/,'')
+        variables_needed[var] = 'text'
+      end
+    end
+    variables_needed.keys
+  end
+  
+  def content_with(locals = {},y = '')
+    haml = Haml::Engine.new(content)
+    haml.render(haml,locals) { y }
+  end
+  
 end

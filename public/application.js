2e477aaf272f4f6597f185f83655b4b5eb5eb4ff
Sam Schenkman-Moore <samsm@samsm.com>
2009-10-30 20:41:06 -0400

    UI refinements

--- public/application.js
+++ public/application.js
@@ -10,7 +10,12 @@ $(document).ready(function(){
       editable.attr('contentEditable',true);
       editable.addClass('editing'); // style for editing
       editable.focus(); // put cursor in element
-      editable.text(''); // clear text
+            
+      // this should only clear if default text is in there
+      if (!editable[0].className.match(/edited/)) {
+        editable.text(''); // clear text
+        editable.addClass('edited')
+      }
       
       // On focus of input, change name= to correspond to p sibling
       editable.parent().children().filter('input').focus(function() {
@@ -19,14 +24,19 @@ $(document).ready(function(){
         // need to sanitize key
         sanitized_key = key.replace(/[0-9 '-]/g,'_').toLowerCase().replace(/[^a-z]/,'').toLowerCase()
         new_name = 'page[' + sanitized_key + ']';
-        console.log(new_name);
+        
         input.attr('name',new_name);
       })
       
       // Move to input field on enter 
       editable.keypress(function(event){
+				
 				// move to field input is cancelled if enter is pressed
 				if (event.which == 13) {
+				  
+				  // Remove editing class -- should add edited class maybe
+				  editable.removeClass('editing');
+				  
 				  // forward to text input
 				  $(editable).parent().children().filter('input').focus();
 				  return false
@@ -34,7 +44,11 @@ $(document).ready(function(){
 				  return event.which
 				}
 			});
-			
+      
+      editable.change(function() {
+        console.log('blur');
+        editable.removeClass('editing');
+      })
       
     });
     

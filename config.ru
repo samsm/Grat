56955384b2f7f97217af102168b22dac61303eee
Sam Schenkman-Moore <samsm@samsm.com>
2009-10-31 15:56:53 -0400

    Database name/host is configurable.

--- config.ru
+++ config.ru
@@ -3,5 +3,10 @@ require 'rubygems'
 require 'rack'
 require 'lib/grat'
 
+Grat.database_conf # uses defaults
+# to override:
+# Grat.database_conf(:host => 'localhost', :database => 'grat_delete')
+
 use Rack::ShowExceptions
+
 run Grat::Application
--- lib/environment.rb
+++ lib/environment.rb
@@ -5,7 +5,17 @@ require 'sass'
 require 'mongomapper'
 MongoMapper.database = 'grat_development'
 
-module Grat ; end
+module Grat
+  
+  def self.database_conf(options = {})
+    if options[:hostname]
+      MongoMapper.connection = XGen::Mongo::Driver::Mongo.new(options[:hostname])
+    end
+    
+    MongoMapper.database = options[:database] || 'grat_development'
+    
+  end
+end
 
 require File.dirname(__FILE__) + '/grat/content'
 require File.dirname(__FILE__) + '/grat/page'

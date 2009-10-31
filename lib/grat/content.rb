8fc57a85e2358d59b09bb2288bb4f1dff71c68e4
Sam Schenkman-Moore <samsm@samsm.com>
2009-10-31 15:15:55 -0400

    'template' doesn't get eroneously saved.

--- lib/grat/content.rb
+++ lib/grat/content.rb
@@ -6,7 +6,7 @@ module Grat::Content
   
   def uneditable_keys
     # url is in here so it can maually be placed at the top of edit form.
-    ["updated_at", "_id", "url", "created_at","content","tags",'template_url']
+    ["updated_at", "_id", "url", "created_at","content","tags",'template_url','template']
   end
   def tags=(val)
     super(val.kind_of?(Array) ? val : val.split(' '))

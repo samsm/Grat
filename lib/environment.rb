e0c76c3dcab0eeeb7d213515d89d6b0af304a88a
Sam Schenkman-Moore <samsm@samsm.com>
2009-11-01 23:25:51 -0500

    Adjusted mongomapper to work with newer driver code

--- lib/environment.rb
+++ lib/environment.rb
@@ -15,16 +15,25 @@ module Grat
   end
   
   def self.database_conf(options = {})
-    @@database_conf = options    
+    @@database_conf = options
   end
   
   def self.database_load
-    require 'mongomapper'
-    if @@database_conf[:hostname]
-      MongoMapper.connection = XGen::Mongo::Driver::Mongo.new(@@database_conf[:hostname])
+    require lib_path + '/grat/mongomapper_patches'
+    connection = if @@database_conf[:hostname]
+      # auth = db.authenticate(my_user_name, my_password)
+      Mongo::Connection.new(@@database_conf[:hostname])
+    else
+      Mongo::Connection.new
     end
     
-    MongoMapper.database = @@database_conf[:database] || 'grat_development'
+    database = connection.db(@@database_conf[:database] || 'grat_development')
+    
+    if @@database_conf[:username] && @@database_conf[:password]
+      database.authenticate(@@database_conf[:username], @@database_conf[:password])
+    end
+    
+    MongoMapper.direct_database = database
     
     require Grat.lib_path + '/grat/content'
     require Grat.lib_path + '/grat/page'

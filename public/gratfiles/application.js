990e6919d1768dd28ec6bc26fec1e0dc9bb4a35d
Sam Schenkman-Moore <samsm@samsm.com>
2010-01-17 17:19:25 -0500

    JQuery Tabs

--- public/gratfiles/application.js
+++ public/gratfiles/application.js
@@ -1,6 +1,6 @@
 // once page is loaded ...
 $(document).ready(function(){
-  
+
   // Collapse fields
   $('.collapsable').each(function() {
     parent_div = $(this);
@@ -19,7 +19,7 @@ $(document).ready(function(){
       parent_div.addClass('activeCollapse');
     }
   })
-  
+
   // Add fields for selected template.
   $('.template select').change(function() {
     // if template has url
@@ -37,26 +37,26 @@ $(document).ready(function(){
       })
     }
   })
-  
+
   // New field button + features
   $('a[href=#new_field]').click(function(){
-    
+
     $('#new_fields').prepend("<div class='new_field'><p class='myEditableText sameaslabel'>Write the title here</p><input name='content[new_field]' value='' /></div>");
-    
+
     // upon click make label editable and other junk associated with customizing the new field
     $('.myEditableText').click(function(){
       editable = $(this);
-      
+
       editable.attr('contentEditable',true);
       editable.addClass('editing'); // style for editing
       editable.focus(); // put cursor in element
-            
+
       // this should only clear if default text is in there
       if (!editable[0].className.match(/edited/)) {
         editable.text(''); // clear text
         editable.addClass('edited')
       }
-      
+
       // On focus of input, change name= to correspond to p sibling
       editable.parent().children().filter('input').focus(function() {
         input = $(this);
@@ -64,19 +64,19 @@ $(document).ready(function(){
         // need to sanitize key
         sanitized_key = key.replace(/[^a-zA-Z]/g,'_').toLowerCase().replace(/[^a-z]/,'').toLowerCase()
         new_name = 'content[' + sanitized_key + ']';
-        
+
         input.attr('name',new_name);
       })
-      
+
       // Move to input field on enter
       editable.keypress(function(event){
-        
+
         // move to field input is cancelled if enter is pressed
         if (event.which == 13) {
-          
+
           // Remove editing class -- should add edited class maybe
           editable.removeClass('editing');
-          
+
           // forward to text input
           $(editable).parent().children().filter('input').focus();
           return false
@@ -84,28 +84,30 @@ $(document).ready(function(){
           return event.which
         }
       });
-      
+
       editable.change(function() {
         console.log('blur');
         editable.removeClass('editing');
       })
-      
+
     });
-    
+
     //  bind an event listener that will be called when
     //  user saves changed content
     $('.editableText').change(function(){
       var newValue = $(this).html();
     });
-    
-    
-    
+
+
+
     return false;
   })
-  
+
   // Beautify default data textarea
   $('#default_content_vars').each(function() {
     this.innerHTML = js_beautify(this.innerHTML, {'indent_size':2});
-  })
-  
+  });
+
+  $('.tabs').tabs();
+
 });
